033b2e10db7bbe40dbe550a295ab9c8391919ffd
Yi Ding <3415065+VaslD@users.noreply.github.com>
2020-03-30 22:55:25 +0800

    Add keys for Auto Photo Backup.

--- UserSettings.cs
+++ UserSettings.cs
@@ -3,8 +3,11 @@
     public static class UserSettings
     {
         public const string EnableSharing = "Global.Share";
-        public const string EnablePhotoSync = "Global.SyncPhotos";
+        public const string EnbalePhotoSharing = "Global.SyncPhotos";
+        public const string AutoBackupPhotos = "Global.SyncPhotosAuto";
         public const string SharingRoot = "Global.ShareRoot";
+        public const string PhotoBackupPrefix = "Global.PhotoRoot";
+        public const string PhotoBackupInterval = "Global.PhotoInterval";
 
         public const string DeviceId = "Cloud.Node.ID";
         public const string DevicePort = "Cloud.Node.Port";
