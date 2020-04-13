0cff8503900316848eed46350454a39e6cdc9144
Yi Ding <3415065+VaslD@users.noreply.github.com>
2020-04-13 20:04:12 +0800

    Add model for Alibaba Cloud.

---
+++ Models/AliYunOSS.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+
+namespace Unishare.Apps.Common.Models
+{
+    [Table("AliYunOSS")]
+    public class AliYunOSS
+    {
+        [PrimaryKey]
+        public string Name { get; set; }
+
+        public string Endpoint { get; set; }
+        public string Bucket { get; set; }
+        public string AccessID { get; set; }
+        public string AccessSecret { get; set; }
+    }
+}
