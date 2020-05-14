93a66bd6137d6560a9756a2b631fde0065c51c72
Yi Ding <appstore@daoyehuo.com>
2020-05-14 23:50:45 +0800

    Helper methods.

---
+++ EnumerableExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Unishare.Apps.Common
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<FileSystemInfo> SortDirectoryFirstByName(this IEnumerable<FileSystemInfo> files)
+        {
+            return files.Where(x => !x.Attributes.HasFlag(FileAttributes.System)
+                                    && !x.Attributes.HasFlag(FileAttributes.Hidden))
+                        .OrderByDescending(x => x.Attributes.HasFlag(FileAttributes.Directory))
+                        .ThenBy(x => x.Name);
+        }
+    }
+}
