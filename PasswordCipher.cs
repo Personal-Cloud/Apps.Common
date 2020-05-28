1cf4fb7c9de0b2da3733b54132d027f4ce32d815
Yi Ding <3415065+VaslD@users.noreply.github.com>
2020-05-28 12:14:27 +0800

    Fix analyzer warnings.

--- PasswordCipher.cs
+++ PasswordCipher.cs
@@ -12,29 +12,34 @@ namespace VaslD.Utility.Cryptography
         public string Password { get; }
         public string Salt { get; }
 
+        public Aes Cipher { get; }
         public ICryptoTransform Encryptor { get; }
         public ICryptoTransform Decryptor { get; }
 
         public PasswordCipher(string password, byte[] salt)
         {
-            if (salt.Length < 16) throw new ArgumentException(nameof(salt));
+            if (!(salt?.Length >= 16)) throw new ArgumentException(null, nameof(salt));
 
             Password = password;
             Salt = Convert.ToBase64String(salt);
-            var cipher = InitCipher();
-            cipher.Padding = PaddingMode.None;
-            Encryptor = cipher.CreateEncryptor();
-            Decryptor = cipher.CreateDecryptor();
+            Cipher = InitCipher();
+            Cipher.Padding = PaddingMode.None;
+            Encryptor = Cipher.CreateEncryptor();
+            Decryptor = Cipher.CreateDecryptor();
         }
 
-        public PasswordCipher(string password, string salt) : this(password, Convert.FromBase64String(salt)) { }
+        public PasswordCipher(string password, string salt) : this(password, Convert.FromBase64String(salt))
+        {
+        }
 
         private Aes InitCipher()
         {
             var salt = new byte[16];
             var bytes = Convert.FromBase64String(Salt);
             Buffer.BlockCopy(bytes, 0, salt, 0, 16);
+#pragma warning disable CA5379 // Breaking change. DO NOT FIX!
             using var keyGen = new Rfc2898DeriveBytes(Password, salt);
+#pragma warning restore CA5379
             var standaloneCipher = Aes.Create();
             standaloneCipher.KeySize = KeySize;
             standaloneCipher.Key = keyGen.GetBytes(KeySize / 8);
@@ -110,6 +115,7 @@ namespace VaslD.Utility.Cryptography
 
         public void Dispose()
         {
+            Cipher.Dispose();
             Encryptor.Dispose();
             Decryptor.Dispose();
         }
--- SQLiteExtensions.cs
+++ SQLiteExtensions.cs
@@ -1,8 +1,9 @@
-﻿using System.Linq;
-
-using SQLite;
+﻿using System;
+using System.Linq;
 
 using NSPersonalCloud.Common.Models;
+
+using SQLite;
 
 namespace NSPersonalCloud.Common
 {
@@ -10,16 +11,22 @@ namespace NSPersonalCloud.Common
     {
         public static bool CheckSetting(this SQLiteConnection database, string key, string value)
         {
+            if (database is null) throw new ArgumentNullException(nameof(database));
+
             return database.Find<KeyValueModel>(key)?.Value == value;
         }
 
         public static string LoadSetting(this SQLiteConnection database, string key)
         {
+            if (database is null) throw new ArgumentNullException(nameof(database));
+
             return database.Find<KeyValueModel>(key)?.Value;
         }
 
         public static bool SaveSetting(this SQLiteConnection database, string key, string value)
         {
+            if (database is null) throw new ArgumentNullException(nameof(database));
+
             return database.InsertOrReplace(new KeyValueModel {
                 Key = key,
                 Value = value
@@ -28,6 +35,8 @@ namespace NSPersonalCloud.Common
 
         public static bool IsStorageNameUnique(this SQLiteConnection database, string name)
         {
+            if (database is null) throw new ArgumentNullException(nameof(database));
+
             return !database.Table<AlibabaOSS>().Any(x => x.Name == name) &&
                    !database.Table<AzureBlob>().Any(x => x.Name == name);
         }
